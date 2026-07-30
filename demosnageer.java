f3044d4386371453bd9dfe22ca68753af76cefd1
saisrikanthterli089 <saisrikanthterli089@gmail.com>
2026-07-30 12:36:36 +0530

    modfiged the demosnageer.java file

--- demosnageer.java
+++ demosnageer.java
@@ -1,3 +1,14 @@
 public class demosnageer {
     
+    public static void main(String args[]){
+        int x =34;
+        demosnageer democlass = new demosnageer();
+        democlass.demo(x);
+    }
+
+
+    public void demo(int x){
+
+        System.out.println(x);
+    }
 }
